a84f0969323547f5855ebe8395727ab4e0fc0bfb
Laurence Zanotti <laurence@primi.com.br>
2023-03-16 14:23:35 -0300

    feat: hello world em web com SALVE MENÓ

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,7 @@
-console.log("salve");
+import express from 'express'
+
+const app = express()
+
+app.get('/', (req, res) => res.send("Salve menó"))
+
+app.listen(3000, () => console.log("Aplicativo rodando no 3000 kkkk"))
