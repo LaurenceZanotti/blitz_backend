96cf9d4d63bfbe31b1337c88b010b113469ac51d
Laurence Zanotti <laurence@primi.com.br>
2023-03-16 15:08:26 -0300

    feat: rotas REST para TODOs

--- src/server.ts
+++ src/server.ts
@@ -4,4 +4,24 @@ const app = express()
 
 app.get('/', (req, res) => res.send("Salve menó"))
 
-app.listen(3000, () => console.log("Aplicativo rodando no 3000 kkkk"))
+app.get('/todos/', (req, res) => {
+    res.send("Listagem de ToDo")
+})
+
+app.post('/todos/', (req, res) => {
+    res.send("Criação de um ToDo")
+})
+
+app.get('/todos/:id', (req, res, id) => {
+    res.send(`Detalhamento do ${req.params.id}`)
+})
+
+app.put('/todos/:id', (req, res, id) => {
+    res.send(`Atualizar o ${req.params.id}`)
+})
+
+app.delete('/todos/:id', (req, res, id) => {
+    res.send(`Deletar o ${req.params.id}`)
+})
+
+app.listen(3000, () => console.log("Aplicativo rodando no http://localhost:3000"))
