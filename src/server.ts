b91da6369f579698506705f3bc99fee4ccad1934
Laurence Zanotti <laurence@primi.com.br>
2023-03-27 16:28:23 -0300

    refactor: add port variable to server.js

--- src/server.ts
+++ src/server.ts
@@ -1,27 +1,32 @@
 import express from 'express'
+import routes from './routes'
+
+const port = 53999
 
 const app = express()
 
-app.get('/', (req, res) => res.send("Salve menó"))
+app.use(routes)
 
-app.get('/todos/', (req, res) => {
-    res.send("Listagem de ToDo")
-})
+// app.get('/', (req, res) => res.send("Salve menó"))
 
-app.post('/todos/', (req, res) => {
-    res.send("Criação de um ToDo")
-})
+// app.get('/todos/', (req, res) => {
+//     res.send("Listagem de ToDo")
+// })
 
-app.get('/todos/:id', (req, res, id) => {
-    res.send(`Detalhamento do ${req.params.id}`)
-})
+// app.post('/todos/', (req, res) => {
+//     res.send("Criação de um ToDo")
+// })
 
-app.put('/todos/:id', (req, res, id) => {
-    res.send(`Atualizar o ${req.params.id}`)
-})
+// app.get('/todos/:id', (req, res, id) => {
+//     res.send(`Detalhamento do ${req.params.id}`)
+// })
 
-app.delete('/todos/:id', (req, res, id) => {
-    res.send(`Deletar o ${req.params.id}`)
-})
+// app.put('/todos/:id', (req, res, id) => {
+//     res.send(`Atualizar o ${req.params.id}`)
+// })
 
-app.listen(3000, () => console.log("Aplicativo rodando no http://localhost:3000"))
+// app.delete('/todos/:id', (req, res, id) => {
+//     res.send(`Deletar o ${req.params.id}`)
+// })
+
+app.listen(port, () => console.log(`Aplicativo rodando no http://localhost:${port}`))
